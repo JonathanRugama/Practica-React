faf095cb1378a9321f74d2cfd2de79331bd91893
Jonathan <jrugama96@gmail.com>
2020-04-13 23:56:05 -0600

    Componentes con multiples estados

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -3,28 +3,40 @@ import axios from "axios";
 
 const Course = ({ match }) => {
 
-    const [state, setState] = useState ({ })
+    const [course, setCourse] = useState ({ })
+    const [comment, setComment] = useState ("Sin comentarios")
    
     useEffect (() => {
         axios.get(`https://my-json-server.typicode.com/jonathanrugama/json-db/courses/${match.params.id}`)
         .then(response => 
-           setState(response.data))
+           setCourse(response.data))
         
     },[])
 
-    console.log(state)
+    const myComment = e => {
+         setComment(e.target.value)
+    }
+
+ 
     //const cursoActual = cursos.filter (c => c.id === parseInt(match.params.id))[0]
     return (
         <div className = "ed-grid m-grid-3">
             {
                
-                state ? (
-                <>
-                    <h1 className="m-cols-3">Curso: {state.title}</h1>
-                     <img className="m-cols-1" src= {state.image} alt="Imagen"/>
-                     <p className="m-cols-2">Profesor {state.profesor}</p>
+                course ? (
+                <div className="ed-grid"> 
+                    <div className="l-block">
+                        <h1 className="m-cols-3">Curso: {course.title}</h1>
+                        <img className="m-cols-1" src= {course.image} alt="Imagen"/>
+                        <p className="m-cols-2">Profesor {course.profesor}</p>
+                    </div>
+                    <div className= "ed-grid">
+                        <h2>Escribe tu comentario</h2>
+                        <input type= "text" placeholder="Escribe..." onChange={myComment.bind(this)}/>
+                        <p>{comment}</p>
+                    </div>
                      
-                </>    
+                </div>    
                 ) : 
                     <h1>El curso no existe</h1>
             }
