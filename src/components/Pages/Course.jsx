247ee0d1d0dcec411e4897af9138e42d1be88cc5
Jonathan <jrugama96@gmail.com>
2020-04-13 22:16:33 -0600

    Accediendo directamente con State

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -4,13 +4,13 @@ import React, {useState} from "react"
 const Course = ({ match }) => {
 
     const [state, setState] = useState ({
-        cursoActual: {
-            "id": 1,
-            "title": "React desde cero",
-            "image": "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
-            "price": 30,
-            "profesor": "Jona Rugama"
-          }
+        
+            id: 1,
+            title: "React desde cero",
+            image: "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
+            price: 30,
+            profesor: "Jona Rugama"
+          
     })
 
     console.log(state)
@@ -19,11 +19,11 @@ const Course = ({ match }) => {
         <div className = "ed-grid m-grid-3">
             {
                
-                state.cursoActual ? (
+                state ? (
                 <>
-                    <h1 className="m-cols-3">Curso: {state.cursoActual.title}</h1>
-                     <img className="m-cols-1" src= {state.cursoActual.image} alt="Imagen"/>
-                     <p className="m-cols-2">Profesor {state.cursoActual.profesor}</p>
+                    <h1 className="m-cols-3">Curso: {state.title}</h1>
+                     <img className="m-cols-1" src= {state.image} alt="Imagen"/>
+                     <p className="m-cols-2">Profesor {state.profesor}</p>
                 </>    
                 ) : 
                     <h1>El curso no existe</h1>
