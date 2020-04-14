2ec9da6cef05f0e0818e958b9fb89bb21f44fa51
Jonathan <jrugama96@gmail.com>
2020-04-13 23:16:47 -0600

    Aprendiendo a usar UseEffect

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -1,24 +1,16 @@
-import React, {useState} from "react"
-
+import React, {useState, useEffect} from "react"
+import axios from "axios";
 
 const Course = ({ match }) => {
 
-    const [state, setState] = useState ({
+    const [state, setState] = useState ({ })
+   
+    useEffect (() => {
+        axios.get(`https://my-json-server.typicode.com/jonathanrugama/json-db/courses/${match.params.id}`)
+        .then(response => 
+           setState(response.data))
         
-            id: 1,
-            title: "React desde cero",
-            image: "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
-            price: 30,
-            profesor: "Jona Rugama"
-          
-    })
-    const changeTitle = (text) => {
-        setState({
-            ...state,     /* Aca le estamos diciendo que pase todas las demas propiedades, y que solamente
-            modifique el titulo */
-            title: text
-        })
-    }
+    },[])
 
     console.log(state)
     //const cursoActual = cursos.filter (c => c.id === parseInt(match.params.id))[0]
@@ -31,7 +23,7 @@ const Course = ({ match }) => {
                     <h1 className="m-cols-3">Curso: {state.title}</h1>
                      <img className="m-cols-1" src= {state.image} alt="Imagen"/>
                      <p className="m-cols-2">Profesor {state.profesor}</p>
-                     <button onClick ={changeTitle.bind(this, "Go desde cero")}>Cambiar titulo</button>
+                     
                 </>    
                 ) : 
                     <h1>El curso no existe</h1>
