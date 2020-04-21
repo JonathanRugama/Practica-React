9a699e3bfc0c8d23e1eb26e8043d04fc01305d2a
Jonathan <jrugama96@gmail.com>
2020-04-20 23:18:47 -0600

    Se reestructura el componente de cursos

--- src/components/Pages/Courses.jsx
+++ src/components/Pages/Courses.jsx
@@ -1,15 +1,18 @@
-import React from "react"
+import React ,{useContext} from "react"
 import axios from "axios";
 import CourseGrid from "../Organisms/CourseGrid";
 import CoursesContext from "../Context/CoursesContext";
 
-const Courses = () => (
-            
-    <CoursesContext.Consumer>
-        {context => <CourseGrid courses ={context.courses} />}
-    </CoursesContext.Consumer>
+const Courses = () => {
+    
+    const {courses} = useContext (CoursesContext) 
+
+    return  (
+      
+         <CourseGrid courses ={courses} />
+    );
         
-);     
+};     
     
 
 
